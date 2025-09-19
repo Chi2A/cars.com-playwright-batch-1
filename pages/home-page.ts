c93e728a4c390fef690101146bec0594d174d592
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-19 18:51:47 -0500

    home page commit

---
+++ pages/home-page.ts
@@ -0,0 +1,13 @@
+
+import { expect, Locator, Page } from "@playwright/test";
+export class HomePage { 
+
+    title: Locator;
+    expectedUrl: Locator;
+    constructor(page:Page){ 
+    this.title = page.locator('h1[class="hero-title "]');
+    }
+    async validateTitle(expectedTitle:string): Promise<void> { 
+        await expect(this.title).toHaveText(expectedTitle)
+    }
+}
