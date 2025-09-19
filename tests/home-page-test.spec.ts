023108a1fc6d32ffca28a307eadf15b680b22571
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-19 18:51:56 -0500

    test commit

---
+++ tests/home-page-test.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test, Page } from "@playwright/test";
+import { HomePage } from "../pages/home-page";
+test("Home Page title validation", async ({ page }) => {
+    let homePage = new HomePage(page);
+    await page.goto('https://www.cars.com');
+    await homePage.validateTitle("Imagine the possibilities");
+    
+})
